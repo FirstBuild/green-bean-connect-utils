b0ea5abcbd271ce30309d5020d9d5624330c95de
MylesCaley <myles.caley@gmail.com>
2015-03-25 15:07:37 -0400

    added exit on commission.sh

--- chillhub-host/commission.sh
+++ chillhub-host/commission.sh
@@ -2,10 +2,12 @@
 
 if [ -z "$1" ]; then
 	echo "usage: sh commission.sh SSID passphrase"
+	exit 1
 fi
 
 if [ -z "$2" ]; then
 	echo "usage: sh commission.sh ssid PASSPHRASE"
+	exit 1
 fi
 
 curl -X POST -H "Content-Type: application/json" -d '{"ssid":"'"$1"'", "passphrase": "'"$2"'"}' http://192.168.10.1/networks
