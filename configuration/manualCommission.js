56ed32604352fbd3d36bd276f32b59243b9ca5ec
MylesCaley <myles.caley@gmail.com>
2015-03-26 19:41:38 -0400

    added manualCommission helper script

---
+++ configuration/manualCommission.js
@@ -0,0 +1,46 @@
+//script to manually commssion green bean connect
+//node manualCommission.js --ssid=1stbuild --ssid_password=firstbuild --url=https://firstbuild-sandbox.firebaseio.com/ --token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InByb3ZpZGVyIjoicGFzc3dvcmQiLCJ1aWQiOiJzaW1wbGVsb2dpbjoxMTMifSwiaWF0IjoxNDI3MjI0MDk4fQ.wnUYgxnppE1_tmPZ96mAQoeTSSmXyN3WjavidizU4Xw
+
+//no error handling
+
+var http = require('http');
+var querystring = require('querystring')
+var argv = require('minimist')(process.argv.slice(2));
+
+commission(argv.url, argv.token, argv.ssid, argv.ssid_password)
+
+function commission(firebaseUrl, firebaseToken, ssidToConnect, ssidPassphrase)
+{
+	console.log("ssid: " + ssidToConnect);
+	console.log("ssid_password: " + ssidPassphrase);
+	console.log("firebase url: " + firebaseUrl);
+	console.log("firebase token: " + firebaseToken);
+
+	console.log("posting token and url...")
+
+	var authDataString = JSON.stringify({"token" : firebaseToken, "url" : firebaseUrl});
+	var ssidDataString = JSON.stringify({"ssid" : ssidToConnect, "passphrase" : ssidPassphrase});
+
+	var authOptions = {hostname: '192.168.10.1',port: 80,path: '/auth',method: 'POST',headers: {'Content-Type': 'application/json'}};
+	var ssidOptions = {hostname: '192.168.10.1',port: 80,path: '/networks',method: 'POST',headers: {'Content-Type': 'application/json'}};
+
+	var ssidReq = http.request(ssidOptions, function(res) {
+	  console.log('SSID POST: ' + res.statusCode);
+	  res.on('data', function (chunk) {
+	    console.log("BODY: " + chunk);
+	  });
+	});
+
+	var authReq = http.request(authOptions, function(res) {
+	  console.log('AUTH POST: ' + res.statusCode);
+	  res.on('data', function (chunk) {
+	    ssidReq.write(ssidDataString);
+		ssidReq.end();
+	  });
+	});
+
+	// write data to request body
+	authReq.write(authDataString);
+	authReq.end();
+	
+}
