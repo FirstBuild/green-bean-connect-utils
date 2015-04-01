4787c2292f9d3dabe3f0f5493a3b9ec0737dc5d8
Christopher M. Baker <mail.chris.baker@gmail.com>
2015-04-01 16:59:01 -0400

    Create firebase-ip.js

---
+++ configuration/firebase-ip.js
@@ -0,0 +1,27 @@
+var os = require('os');
+var Firebase = require('firebase');
+var chillhub = require('./chillhub.json');
+var ref = new Firebase('https://firstbuild-sandbox.firebaseio.com');
+
+function ip_address(interface) {
+  var items = os.networkInterfaces()[interface] || [];
+
+  return items
+    .filter(function(item) {
+      return item.family.toLowerCase() == 'ipv4';
+    })
+    .map(function(item) {
+      return item.address;
+    })
+    .shift();
+}
+
+ref.authWithCustomToken(chillhub.token, function(err, auth) {
+  if (err) return console.error(err);
+
+  var content = { ip_address: ip_address('wlan0') || null };
+
+  ref.child('users').child(auth.uid).update(content, function(err) {
+    process.exit(0);
+  });
+});
