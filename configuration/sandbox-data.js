3e8ff7326d7803905316e35db29194d040d19a47
Christopher M. Baker <mail.chris.baker@gmail.com>
2015-04-02 10:01:14 -0400

    Update sandbox-data.js

--- configuration/sandbox-data.js
+++ configuration/sandbox-data.js
@@ -1,15 +1,37 @@
-// installation:
-// $ npm install firebase
+var fs = require('fs');
 
-// usage:
-// $ node sandbox-data.js <email> <password>
+function data(email, password) {
+  var Firebase = require('firebase');
+  var ref = new Firebase('https://firstbuild-sandbox.firebaseio.com');
+  var credentials = { email: email, password: password };
 
-var Firebase = require('firebase');
-var ref = new Firebase('https://firstbuild-sandbox.firebaseio.com');
-var credentials = { email: process.argv[2], password: process.argv[3] };
-
-ref.authWithPassword(credentials, function(err, auth) {
-  ref.child('users').child(auth.uid).once('value', function(snapshot) {
-    console.log(JSON.stringify(snapshot.val(), null, 2));
+  ref.authWithPassword(credentials, function(err, auth) {
+    if (err) {
+      console.error('Failed to login with credentials:', err);
+    }
+    else if (auth) {
+      ref.child('users').child(auth.uid).once('value', function(snapshot) {
+        console.log(JSON.stringify(snapshot.val(), null, 2));
+      });
+    }
+    else {
+      console.error('Failed to login with credentials!');
+      console.error('Make sure you entered your email and password correctly.');
+    }
   });
+}
+
+fs.exists('./node_modules/firebase', function(exists) {
+  if (exists) {
+    if (process.argv.length == 4) {
+      data(process.argv[2], process.argv[3]);
+    }
+    else {
+      console.error('usage: node sandbox-data.js <email> <password>');
+    }
+  }
+  else {
+    console.error('Could not find firebase package!');
+    console.error('Did you run `npm install firebase`?');
+  }
 });
